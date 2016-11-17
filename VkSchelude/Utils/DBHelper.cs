a585aefbc6156bcfa2f907e7487a61b59a8fa253
Sysoenko N.A. <stels_unit111@mail.ru>
2016-11-17 20:26:29 +0500

    Сделал проверку прав и много всего, связанного с sql. Работает пока только !помощь

---
+++ VkSchelude/Utils/DBHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkSchelude.Utils
+{
+    public static class DBHelper
+    {
+        public static string GetInternalSQLRequest(int Id)
+        {
+            if (Authorize.connection.State != System.Data.ConnectionState.Open)
+                Authorize.connection.Open();
+            return new SqlCommand($"SELECT SqlCommand FROM sys_SqlCommands WHERE Id = {Id}", Authorize.connection).ExecuteScalar().ToString();
+        }
+        public static object GetSingleObject(string Request, Dictionary <string,object> Parameters = null)
+        {
+            if (Authorize.connection.State != System.Data.ConnectionState.Open)
+                Authorize.connection.Open();
+            if (Parameters == null)
+                Parameters = new Dictionary<string, object>();
+            SqlCommand cmd = new SqlCommand(Request, Authorize.connection);
+            foreach(var item in Parameters)
+            {
+                cmd.Parameters.AddWithValue(item.Key, item.Value);
+            }
+            return cmd.ExecuteScalar().ToString();
+        }
+        public static Dictionary<object, object> GetDictionary(string Request, Dictionary<string, object> Parameters = null)
+        {
+            if (Authorize.connection.State != System.Data.ConnectionState.Open)
+                Authorize.connection.Open();
+            if (Parameters == null)
+                Parameters = new Dictionary<string, object>();
+            SqlCommand cmd = new SqlCommand(Request, Authorize.connection);
+            foreach (var item in Parameters)
+            {
+                cmd.Parameters.AddWithValue(item.Key, item.Value);
+            }
+            Dictionary<object, object> result = new Dictionary<object, object>();
+            var reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                result.Add(reader.GetValue(0), reader.GetValue(1));
+            }
+            return result;
+        }
+    }
+}
--- VkSchelude/groupBot.cs
+++ VkSchelude/groupBot.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 using VkNet;
 using VkNet.Model.RequestParams;
 using VkSchelude.Utils;
+using System.Text.RegularExpressions;
+using VkSchelude.Utils;
 
 namespace VkSchelude
 {
     class groupBot
     {
+        private string patternStart = @"^!";
         public static void Start()
         {
             while (true)
@@ -25,20 +28,36 @@ namespace VkSchelude
                 {
                     foreach (var dialog in dialogs.Messages)
                     {
-                        if (dialog.Body.Contains("&"))
+                        if (Regex.IsMatch(dialog.Body, @"^!"))
                         {
-                            if (dialog.Body.Contains("&расписание"))
+                            if (Authorize.connection.State != System.Data.ConnectionState.Open)
+                                Authorize.connection.Open();
+                            string selectRights = DBHelper.GetInternalSQLRequest(1);
+                            int currentRightsId = int.Parse(DBHelper.GetSingleObject(selectRights, new Dictionary<string, object> { { "@UserId", dialog.UserId } }).ToString());
+                            selectRights = DBHelper.GetInternalSQLRequest(5);
+                            var aa = DBHelper.GetDictionary(selectRights, new Dictionary<string, object> { { "@CurrentRightsId", currentRightsId } });
+                            string outString = String.Empty;
+                            foreach(var item in aa)
                             {
-                                var dates = dialog.Body.Replace("&расписание", "").Trim();
-                                var parsedDates = dates.Split(',').ToList();
-                                foreach (var date in parsedDates)
-                                {
-                                    var message = Schedule.buildSchedule(date);
-                                    if (message != String.Empty)
-                                        Send.SendInMessages(Authorize.vkGroup, message, dialog.UserId);
-                                }
+                                outString += $"!{item.Key} - {item.Value}\n";
                             }
+                            if (!String.IsNullOrEmpty(outString))
+                                Send.SendInMessages(Authorize.vkGroup, outString, dialog.UserId);
                         }
+                        //if (dialog.Body.Contains("&"))
+                        //{
+                        //    if (dialog.Body.Contains("&расписание"))
+                        //    {
+                        //        var dates = dialog.Body.Replace("&расписание", "").Trim();
+                        //        var parsedDates = dates.Split(',').ToList();
+                        //        foreach (var date in parsedDates)
+                        //        {
+                        //            var message = Schedule.buildSchedule(date);
+                        //            if (message != String.Empty)
+                        //                Send.SendInMessages(Authorize.vkGroup, message, dialog.UserId);
+                        //        }
+                        //    }
+                        //}
                     }
                 }
                 Thread.Sleep(10000);
